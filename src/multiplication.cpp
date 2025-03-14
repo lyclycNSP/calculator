a001577dbcfecad284ec0cc12a7c533b99fb407b
Elizabeththh <elizabeththhh@gmail.com>
2025-03-14 08:40:22 +0800

    add some function related to multiplication

--- src/multiplication.cpp
+++ src/multiplication.cpp
@@ -3,39 +3,43 @@
 
 // this function handles two positive integers's multiplication
 // the positiveness is guarantee by the calculation func's preprosession
-string integer_multiplication(string fir, string sec)
-{
-    string ans{};
-    if(is_zero(fir) or is_zero(sec))
-        return "0";
+// string multiplication::integer_multiplication(string fir, string sec)
+// {
+//     string ans{};
+//     if(calculator::is_zero(fir) or calculator::is_zero(sec))
+//         return "0";
 
-    string longnum = fir.length() >= sec.length() ? fir : sec;
-    string shortnum = fir.length() < sec.length() ? fir : sec;
+//     string longnum = fir.length() >= sec.length() ? fir : sec;
+//     string shortnum = fir.length() < sec.length() ? fir : sec;
 
-    reverse(longnum.begin(), longnum.end());
-    reverse(shortnum.begin(), shortnum.end());
+//     reverse(longnum.begin(), longnum.end());
+//     reverse(shortnum.begin(), shortnum.end());
 
 
-    ans = reverse_multiplication(longnum, shortnum);
-    return ans;
-}
+//     ans = reverse_multiplication(longnum, shortnum);
+//     return ans;
+// }
 
-// this function handles two reversed integer's multiplication
-string multiplication::reverse_multiplication(string longnum, string shortnum)
-{
-    size_t shortlen = shortnum.length();    
-    size_t longlen = longnum.length();
+// // this function handles two reversed integer's multiplication
+// string multiplication::reverse_multiplication(string longnum, string shortnum)
+// {
+//     string ans{};
+//     size_t shortlen = shortnum.length();    
+//     size_t longlen = longnum.length();
 
-    int result{};
-    int carry{};
+//     int result{};
+//     int carry{};
 
-    for(size_t i{}; i < shortlen; i++)
-    {
-        for(size_t j{}; j < longlen; j++)
-        {
-            int sum = (longnum[i] - '0') * (shortnum[j] - '0') + carry;
-            result = sum % 10;
-            carry = sum / 10;
-        }
-    }
-}
+//     for(size_t i{}; i < shortlen; i++)
+//     {
+//         for(size_t j{}; j < longlen; j++)
+//         {
+//             int sum = (longnum[i] - '0') * (shortnum[j] - '0') + carry;
+//             result = sum % 10;
+//             carry = sum / 10;
+//         }
+//     }
+
+//     // ...... unfinished
+//     return ans;
+// }
