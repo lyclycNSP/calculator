40c68e8c758e85a1c55c831f95e60910c99b69eb
Elizabeththh <elizabeththhh@gmail.com>
2025-03-14 09:00:57 +0800

    add build on windows

---
+++ build-win/CMakeFiles/calculator.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calculator.dir/src/addition.cpp.obj"
+  "CMakeFiles/calculator.dir/src/addition.cpp.obj.d"
+  "CMakeFiles/calculator.dir/src/calculation.cpp.obj"
+  "CMakeFiles/calculator.dir/src/calculation.cpp.obj.d"
+  "CMakeFiles/calculator.dir/src/calculator.cpp.obj"
+  "CMakeFiles/calculator.dir/src/calculator.cpp.obj.d"
+  "CMakeFiles/calculator.dir/src/division.cpp.obj"
+  "CMakeFiles/calculator.dir/src/division.cpp.obj.d"
+  "CMakeFiles/calculator.dir/src/main.cpp.obj"
+  "CMakeFiles/calculator.dir/src/main.cpp.obj.d"
+  "CMakeFiles/calculator.dir/src/multiplication.cpp.obj"
+  "CMakeFiles/calculator.dir/src/multiplication.cpp.obj.d"
+  "CMakeFiles/calculator.dir/src/subtraction.cpp.obj"
+  "CMakeFiles/calculator.dir/src/subtraction.cpp.obj.d"
+  "calculator.exe"
+  "calculator.exe.manifest"
+  "calculator.pdb"
+  "libcalculator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calculator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
